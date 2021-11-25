7021d26d45ce9ca8349b55b70e83eb426b6b5b45
Adam Siekierski <adam.siekierski@appunite.com>
2021-11-25 22:39:57 +0100

    feat: setup the dev environment

---
+++ prisma/migrations/20211125213249_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
