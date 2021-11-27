7de2e22f84720a94c9df1b93f76e0979522fdfb0
ejer <ejer.priv@wp.pl>
2021-11-27 18:30:01 +0100

    feat: add letter, author and destination seeding using faker.js

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Author, Destination, Letter } from "@prisma/client";
 import { hash } from "../lib/utils/bcryptHash";
+import faker from "faker";
 const prisma = new PrismaClient();
+
+faker.locale = "pl";
 
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@admin.com";
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
@@ -16,6 +19,58 @@ async function main() {
     },
   });
   console.log({ admin });
+
+  let authorsList = [];
+  let destinationsList = [];
+  let lettersList = [];
+
+  for (let i = 1; i <= 20; i++) {
+    const name = faker.name.firstName() + " " + faker.name.lastName();
+    const destinationId = faker.datatype.uuid();
+
+    destinationsList.push({
+      id: destinationId,
+      name,
+    });
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    const classId = i + (i === 1 ? "A" : i === 2 ? "B" : "C");
+
+    for (let i = 1; i <= 30; i++) {
+      const registerNumber = i;
+      const authorId = faker.datatype.uuid();
+      const letterAmount = Math.floor(Math.random() * 4) + 1;
+
+      for (let i = 0; i < letterAmount; i++) {
+        const letterId = faker.datatype.uuid();
+
+        lettersList.push({
+          id: letterId,
+          authorId,
+          destinationId: faker.random.arrayElement(destinationsList).id,
+        });
+      }
+
+      authorsList.push({
+        id: authorId,
+        classId,
+        registerNumber,
+      });
+    }
+  }
+
+  await prisma.author.createMany({
+    data: authorsList,
+  });
+
+  await prisma.destination.createMany({
+    data: destinationsList,
+  });
+
+  await prisma.letter.createMany({
+    data: lettersList,
+  });
 }
 
 main()
