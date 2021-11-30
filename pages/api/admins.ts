48ea8ddecfcf390a93ce42ac357de3e4adf7cb4e
AleksanderSkubala <alexskubala@gmail.com>
2021-11-30 10:29:53 +0100

    fix: adding auth also to the api route

--- pages/api/admins.ts
+++ pages/api/admins.ts
@@ -3,6 +3,7 @@ import { withApiMethods } from "../../lib/withApiMethods";
 import { prisma } from "../../lib/db";
 import { Asserts, object, string } from "yup";
 import { hash } from "../../lib/utils/bcryptHash";
+import { withApiAuth } from "../../lib/auth/withApiAuth";
 
 export const adminSchema = object({
   email: string().required("E-Mail is required").email("This is not an email"),
@@ -11,27 +12,29 @@ export const adminSchema = object({
 
 export type AdminSchemaType = Asserts<typeof adminSchema>;
 
-export default withApiMethods({
-  GET: async (req, res) => {
-    const allAdmins = await prisma.admin.findMany();
-    res.json({ allAdmins });
-  },
-  POST: withApiValidation(adminSchema, async (req, res) => {
-    const { email, password } = req.body;
+export default withApiAuth(
+  withApiMethods({
+    GET: async (req, res) => {
+      const allAdmins = await prisma.admin.findMany();
+      res.json({ allAdmins });
+    },
+    POST: withApiValidation(adminSchema, async (req, res) => {
+      const { email, password } = req.body;
 
-    try {
-      const passwordHash = await hash(password);
+      try {
+        const passwordHash = await hash(password);
 
-      const newAdmin = await prisma.admin.create({
-        data: {
-          email,
-          password: passwordHash,
-        },
-      });
+        const newAdmin = await prisma.admin.create({
+          data: {
+            email,
+            password: passwordHash,
+          },
+        });
 
-      return res.json({ message: "Admin has been added successfully", newAdmin });
-    } catch {
-      return res.status(500).json({ message: "Internal server error" });
-    }
+        return res.json({ message: "Admin has been added successfully", newAdmin });
+      } catch {
+        return res.status(500).json({ message: "Internal server error" });
+      }
+    }),
   }),
-});
+);
