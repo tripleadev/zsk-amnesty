f3fe60fb8a0a6a1d3efbb013ab3d184b8786b914
Adam Siekierski <adam.siekierski@appunite.com>
2021-12-05 14:48:36 +0100

    hotfix: stats

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -4,16 +4,21 @@ import { statsAreExpired, generateStats, updateStats } from "../../lib/stats/sta
 
 export default withApiMethods({
   GET: async (_req, res) => {
-    const stats = await prisma.stats.findFirst();
+    try {
+      const stats = await prisma.stats.findFirst({});
 
-    if (statsAreExpired(stats)) {
-      const newStats = await generateStats();
+      if (statsAreExpired(stats)) {
+        const newStats = await generateStats();
 
-      await updateStats(newStats);
+        await updateStats(newStats);
 
-      return res.json(newStats);
-    } else {
-      return res.json(stats?.stats);
+        return res.json(newStats);
+      } else {
+        return res.json(stats?.stats);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
     }
   },
 });
