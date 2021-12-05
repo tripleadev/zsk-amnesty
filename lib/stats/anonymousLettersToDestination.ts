4ffde09ecffbabf389d3e5aeaf3d4538fddc63f4
ejer <ejer.priv@wp.pl>
2021-12-05 22:40:57 +0100

    fix: anonymous letters count to authored letters

--- lib/stats/anonymousLettersToDestination.ts
+++ lib/stats/anonymousLettersToDestination.ts
@@ -1,5 +1,5 @@
 import { Letter, Author } from "@prisma/client";
 
-export const anonymousLettersTo = (letters: Letter[], anons: Author[]) => {
+export const anonymousLettersToDestination = (letters: Letter[], anons: Author[]) => {
   return letters.filter((letter) => anons.some((author) => author.id === letter.authorId)).length;
 };
