55951f7aeb6eca7d8c1b0ef09fa05e682d9f51ec
Adam Siekierski <adam.siekierski@appunite.com>
2021-12-05 16:20:42 +0100

    fix: wtf prisma

--- prisma/migrations/20211205145405_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Stat" (
-    "id" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Stat_pkey" PRIMARY KEY ("id")
-);
