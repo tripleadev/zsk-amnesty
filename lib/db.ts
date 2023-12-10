907f311ef70a278f2697273f86d0e075d0ef72da
Adam Siekierski <adam.siekierski@appunite.com>
2023-12-10 23:20:28 +0100

    bump typesctipt

--- lib/db.ts
+++ lib/db.ts
@@ -5,10 +5,13 @@ declare global {
   var prisma: PrismaClientType | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+export let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
